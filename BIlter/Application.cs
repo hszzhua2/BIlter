22301044a64023be4ee190e3d13a50332efc8c71
Zefeng Zhuang <98097238+hszzhua2@users.noreply.github.com>
2023-08-07 15:56:43 +0800

    666

--- BIlter/Application.cs
+++ BIlter/Application.cs
@@ -14,6 +14,8 @@ using RibbonItem = Autodesk.Revit.UI.RibbonItem;
 using BIlter.Commands;
 using System.Windows.Shapes;
 using System.Resources.Extensions;
+using Nice3point.Revit.Toolkit.External;
+using Nice3point.Revit.Extensions;
 
 namespace BIlter
 {
@@ -24,7 +26,7 @@ namespace BIlter
         {
             return Result.Succeeded;
         }
-        /*private static void CreateLogger()
+        private static void CreateLogger()
         {
             const string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
 
@@ -38,7 +40,7 @@ namespace BIlter
                 var e = (Exception)args.ExceptionObject;
                 Log.Fatal(e, "Domain unhandled exception");
             };
-        }*/
+        }
 
         private const string _tab = "BIlter";
         private const string panelName1 = "BIlter";
@@ -48,15 +50,35 @@ namespace BIlter
             application.CreateRibbonTab(_tab); 
             var panelFamily = application.CreateRibbonPanel(_tab, panelName1);
 
-            #region MM
-            var buttomMM = new PushButtonData("Material Manager", "Material\nList and \n Manager", typeof(Application).Assembly.Location, "BIlter.Commands.MaterialsCommand");
-            var buttomMMPlus = panelFamily.AddItem(buttomMM);
+            #region MM 材质管理
+            var buttomMM = new PushButtonData("Material Manager", "Material Manager", typeof(Application).Assembly.Location, "BIlter.Commands.MaterialsCommand");
+
+            var buttomMMPlus = panelFamily.AddItem(buttomMM) as PushButton;
+            buttomMMPlus.SetLargeImage("/BIlter;component/Resources/Icons/Windows32.png");
+            buttomMMPlus.SetImage("/BIlter;component/Resources/Icons/Windows16.png");
             RibbonToolTip toolTip = new RibbonToolTip()
-            {Title = "Material Manager",
-             Content = "", 
-             ExpandedContent = "",
-             ExpandedVideo = new Uri("C:\\Program Files\\Autodesk\\Revit 2023\\videos\\tooltip.mp4"),};
+            { 
+                Title = "Material Manager",
+                Content = "A manager for materials editing, creating, deleting, importing and exporting.",
+                ExpandedContent = "A manager for materials editing, creating, deleting, importing and exporting. The color editor and creator are support and it is only supported to edit appearance color.",
+                ExpandedVideo = new Uri("C:\\Program Files\\Autodesk\\Revit 2023\\videos\\tooltip.mp4"),
+            };
             SetRibbonItemToolTip(buttomMMPlus, toolTip);
+            #endregion
+
+            #region CV 检查版本
+            var buttomCV = new PushButtonData("Check Version", "Check Version", typeof(Application).Assembly.Location, "BIlter.Commands.GetRvtVersion");
+
+            var buttomCVPlus = panelFamily.AddItem(buttomCV) as PushButton;
+            buttomCVPlus.SetLargeImage("/BIlter;component/Resources/Icons/Version32.png");
+            buttomCVPlus.SetImage("/BIlter;component/Resources/Icons/Version16.png");
+            RibbonToolTip toolTip2 = new RibbonToolTip()
+            {
+                Title = "Check File Version",
+                Content = "To check version of an .rvt file.",
+                ExpandedContent = ".",
+            };
+            SetRibbonItemToolTip(buttomCVPlus, toolTip2);
             #endregion
 
 
